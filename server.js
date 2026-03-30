0c9d6b23e32039c9a56a81ef940d68eba3ab75c6
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-03-30 13:16:57 +0200

    server.js fix

--- server.js
+++ server.js
@@ -5,77 +5,104 @@ import express from 'express'
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
 import { Liquid } from 'liquidjs';
 
+
+console.log('Hieronder moet je waarschijnlijk nog wat veranderen')
+// Doe een fetch naar de data die je nodig hebt
+const [documentsRes, eventsRes, newsRes, nominationsRes, themesRes, categoriesRes, contactRes] = await Promise.all([
+   fetch('https://fdnd-agency.directus.app/items/adconnect_documents'),
+   fetch('https://fdnd-agency.directus.app/items/adconnect_events'),
+   fetch('https://fdnd-agency.directus.app/items/adconnect_news'),
+   fetch('https://fdnd-agency.directus.app/items/adconnect_nominations'),
+   fetch('https://fdnd-agency.directus.app/items/adconnect_themes'),
+   fetch('https://fdnd-agency.directus.app/items/adconnect_categories'),
+   fetch('https://fdnd-agency.directus.app/items/adconnect_contact'),
+])
+
+const documents = await documentsRes.json()
+const events = await eventsRes.json()
+const news = await newsRes.json()
+const nominations = await nominationsRes.json()
+const themes = await themesRes.json()
+const categories = await categoriesRes.json()
+const contact = await contactRes.json()
+
+// Controleer eventueel de data in je console
+// (Let op: dit is _niet_ de console van je browser, maar van NodeJS, in je terminal)
+// console.log(apiResponseJSON)
+
+
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
 const app = express()
 
 // Maak werken met data uit formulieren iets prettiger
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
 app.use(express.static('public'))
 
 // Stel Liquid in als 'view engine'
-const engine = new Liquid()
-app.engine('liquid', engine.express())
+const engine = new Liquid();
+app.engine('liquid', engine.express());
 
 // Stel de map met Liquid templates in
 // Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
 app.set('views', './views')
 
-
-console.log('Let op: Er zijn nog geen routes. Voeg hier dus eerst jouw GET en POST routes toe.')
-
-/*
-// Zie https://expressjs.com/en/5x/api.html#app.get.method over app.get()
-app.get(…, async function (request, response) {
-  
-  // Zie https://expressjs.com/en/5x/api.html#res.render over response.render()
-  response.render(…)
+// Maak een GET route voor de index (meestal doe je dit in de root, als /)
+app.get('/', async function (request, response) {
+   // Render index.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   response.render('index.liquid', { documents: documents.data, events: events.data, news: news.data, nominations: nominations.data, themes: themes.data, categories: categories.data, contact: contact.data })
 })
-*/
 
-/*
-// Zie https://expressjs.com/en/5x/api.html#app.post.method over app.post()
-app.post(…, async function (request, response) {
-
-  // In request.body zitten alle formuliervelden die een `name` attribuut hebben in je HTML
-  console.log(request.body)
-
-  // Via een fetch() naar Directus vullen we nieuwe gegevens in
-
-  // Zie https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch over fetch()
-  // Zie https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify over JSON.stringify()
-  // Zie https://docs.directus.io/reference/items.html#create-an-item over het toevoegen van gegevens in Directus
-  // Zie https://docs.directus.io/reference/items.html#update-an-item over het veranderen van gegevens in Directus
-  const fetchResponse = await fetch(…, {
-    method: …,
-    body: JSON.stringify(…),
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8'
-    }
-  })
-
-  // Als de POST niet gelukt is, kun je de response loggen. Sowieso een goede debugging strategie.
-  // console.log(fetchResponse)
-
-  // Eventueel kun je de JSON van die response nog debuggen
-  // const fetchResponseJSON = await fetchResponse.json()
-  // console.log(fetchResponseJSON)
-
-  // Redirect de gebruiker daarna naar een logische volgende stap
-  // Zie https://expressjs.com/en/5x/api.html#res.redirect over response.redirect()
-  response.redirect(303, …)
+app.get('/ad-talent-award', async function (request, response) {
+   // Render adTalentAward.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   response.render('talentAward.liquid', { documents: documents.data, events: events.data, news: news.data, nominations: nominations.data, themes: themes.data, categories: categories.data, contact: contact.data })
 })
-*/
 
+app.get('/nominee/:naam', async function (request, response) {
+   // Render nominee.liquid uit de Views map
+   const naam = request.params.naam
+   // Haal dash uit volledige naam
+   const correcteNaam = naam.replaceAll('-', ' ')
+   // Zoek in array op naam
+   const fetchedNaam = nominations.data.find(item => item.title === correcteNaam)
+
+   response.render('nominee.liquid', { nomination: fetchedNaam })
+})
+
+app.get('/opleidingsprofielen', async function (request, response) {
+   // Render opleidingsprofielen.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   response.render('opleidingsprofielen.liquid', { documents: documents.data, events: events.data, news: news.data, nominations: nominations.data, themes: themes.data, categories: categories.data, contact: contact.data })
+})
+
+app.get('/contact', async function (request, response) {
+   // Render opleidingsprofielen.liquid uit de Views map
+   // Geef hier eventueel data aan mee
+   response.render('contact.liquid', { documents: documents.data, events: events.data, news: news.data, nominations: nominations.data, themes: themes.data, categories: categories.data, contact: contact.data })
+})
+
+// Maak een POST route voor de index; hiermee kun je bijvoorbeeld formulieren afvangen
+// Hier doen we nu nog niets mee, maar je kunt er mee spelen als je wilt
+app.post('/', async function (request, response) {
+   // Je zou hier data kunnen opslaan, of veranderen, of wat je maar wilt
+   // Er is nog geen afhandeling van een POST, dus stuur de bezoeker terug naar /
+   response.redirect(303, '/')
+})
+
+app.use((req, res) => {
+  res.status(404).send("Pagina bestaat niet!")
+})
 
 // Stel het poortnummer in waar Express op moet gaan luisteren
-// Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
+// Lokaal is dit poort 8000, als dit ergens gehost wordt, is het waarschijnlijk poort 80
 app.set('port', process.env.PORT || 8000)
 
-// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+// Start Express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
-  // Toon een bericht in de console
-  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
+   // Toon een bericht in de console en geef het poortnummer door
+   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
